f1d5a80bb04ca6c5f4e1097e2908c51f1a5e6eaf
tiberiuiancu <tiberiu.iancu@gmail.com>
2020-06-09 18:46:25 +0200

    little change in training

--- training.py
+++ training.py
@@ -5,10 +5,17 @@ import numpy as np
 import pickle
 import time
 
-np.random.seed(0)
 
-TRAINING_ROUNDS = 1 * 50 * 1000
+def save_values(players):
+    # save to not lose progress
+    for i, player in enumerate(players):
+        with open('savefile' + str(i) + '.pickle', 'wb') as f:
+            pickle.dump(player.qvalues, f)
+
+
+TRAINING_ROUNDS = 1 * 5000 * 1000
 PRINT_EVERY = 100
+SAVE_EVERY = 1000
 LOAD = False
 
 if __name__ == '__main__':
@@ -27,6 +34,9 @@ if __name__ == '__main__':
 
     start_time = time.time()
     for i in range(TRAINING_ROUNDS):
+        if i % SAVE_EVERY == SAVE_EVERY - 1:
+            print('SAVING')
+            save_values(players)
 
         if i % PRINT_EVERY == PRINT_EVERY - 1:
             remaining = int((time.time() - start_time) / i * (TRAINING_ROUNDS - i))
@@ -34,19 +44,3 @@ if __name__ == '__main__':
                                               (remaining % 3600) // 60,
                                               remaining % 3600 % 60))
         game.start_round()
-
-    # save to not lose progress
-    for i, player in enumerate(players):
-        with open('savefile' + str(i) + '.pickle', 'wb') as f:
-            pickle.dump(player.qvalues, f)
-
-    for player in players:
-        player.training = False
-
-    # play a round with these players
-    game = Game(
-        players=players[:-1] + [HumanPlayer('human')],
-        dealer_type=PerfectDealer,
-        verbose=True
-    )
-
